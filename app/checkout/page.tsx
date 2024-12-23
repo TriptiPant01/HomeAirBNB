550ef2432fca19f5303a7a0974bc53eb2cfbb7dd
Tripti Pant <itsmetripti20@gmail.com>
2024-12-23 09:56:29 +0545

    fix: WIP

---
+++ app/checkout/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CheckoutPage() {
+  return (
+    <div>
+      <h1 className="text-7xl">CheckoutPage</h1>
+    </div>
+  )
+}
---
+++ app/favourite/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FavouritePage() {
+  return (
+    <div>
+      <h1 className="text-7xl">Fourite</h1>
+    </div>
+  )
+}
